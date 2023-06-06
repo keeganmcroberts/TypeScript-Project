2607fd9e11f707b8fac01fb3c5e2b872dffd5b35
Keegan McRoberts <keegan.mcroberts@gmail.com>
2023-06-06 11:17:01 -0600

    simple css to set up to do list

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
+let name: string;
+let age: number | string;
+let isStudent: boolean;
+let hobbies:string[];
+let role:[number, string];
+let anything:any;
+
+name = "Keegan"
+
+// function printName(name:string){
+//   console.log(name)
+// }
+
+// printName(name)
+
+let printName: (name:string)=>void;
+//void returns undefined
+let printName2: (name:string)=>never; 
+//never returns nothing 
+
+
+
+
+// let lotsOfPeople:Person[]
+
+interface Person {
+  name:string;
+  age?:number;
+}
+
+
+
+let person: Person = {
+    name: "Keegan",
+    age: 26,
+  }
+
+  type X = {
+    a:string;
+    b:number;
+  }
+
+
+
+  
+  const App:React.FC = () => {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <span className="heading">ToDo</span>
+     
     </div>
   );
 }
